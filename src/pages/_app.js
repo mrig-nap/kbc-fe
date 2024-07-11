0e024906c4adb4a20031019eb63bec4fd9f9b914
Mriganshu Singh <msingh@newageproducts.com>
2024-07-11 17:56:42 +0530

    updated server url

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
 import "@/styles/globals.css";
-import { SocketProvider } from '../contexts/SocketContext';
 
 export default function App({ Component, pageProps }) {
   return(
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <Component {...pageProps} />
   ) 
 }
